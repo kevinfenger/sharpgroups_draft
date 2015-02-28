a2aa1280df45397f9ffaa43d632cc716575d4632
kevinfenger <kevindfenger@gmail.com>
2015-02-28 16:35:45 -0500

    adding draggable and minimizable panels

--- public/modules/drafts/controllers/auction.client.controller.js
+++ public/modules/drafts/controllers/auction.client.controller.js
@@ -5,9 +5,15 @@ angular.module('drafts').controller('AuctionController', ['$scope', '$http', '$w
 	$scope.authentication = Authentication;
         if (!$scope.authentication.user) 
             $location.path('signin');
+        var minHeight = '350px'; 
         $scope.increaseBidAmount = 10;  
         $scope.sharpsBeforeBid = 2500;  
         $scope.sharpsAfterBid = 2500;  
+        $scope.chatCollapsed = false;
+        $scope.infoCollapsed = false; 
+        $scope.chatBodyHeight = minHeight; 
+        $scope.chatWrapperHeight = minHeight; 
+        $scope.infoWrapperHeight = minHeight; 
         $scope.drafters = [];
         $scope.chats = []; 
         $scope.username = $scope.authentication.user.firstName + ' ' + $scope.authentication.user.lastName;
@@ -37,6 +43,24 @@ angular.module('drafts').controller('AuctionController', ['$scope', '$http', '$w
                 }
             }  
         };
+        $scope.collapseChat = function() { 
+           $scope.chatCollapsed = true;
+           $scope.chatWrapperHeight = '0px'; 
+           $scope.chatBodyHeight = '0px'; 
+        }; 
+        $scope.maxChat = function() { 
+           $scope.chatCollapsed = false; 
+           $scope.chatWrapperHeight = minHeight; 
+           $scope.chatBodyHeight = minHeight; 
+        }; 
+        $scope.collapseInfo = function() { 
+           $scope.infoCollapsed = true;
+           $scope.infoWrapperHeight = '0px'; 
+        }; 
+        $scope.maxInfo = function() { 
+           $scope.infoCollapsed = false; 
+           $scope.infoWrapperHeight = minHeight; 
+        }; 
     } 
 ])
 .directive('sgScrollToBottom', function($interval) { 
@@ -51,4 +75,57 @@ angular.module('drafts').controller('AuctionController', ['$scope', '$http', '$w
           }); 
    }
    return { link : link }; 
+})
+.directive('draggableElement', function($document) { 
+    function link(scope, element, attrs) { 
+      var startX = 0, startY = 0, x = 0, y = 0, headingHeight=35;
+
+      element.css({
+       position: 'relative',
+      });
+
+      function goodCoordinates(curY, eleTop) { 
+          return (curY - eleTop) < headingHeight;
+      }
+      element.on('mouseover', function(event) { 
+          var curY = event.pageY;
+          var eleTop = element[0].getBoundingClientRect().top; 
+          if (goodCoordinates(curY, eleTop)) { 
+             element.css({
+               cursor: 'move'
+             });
+          }
+          else { 
+             element.css({
+               cursor: 'auto'
+             });
+          }
+      });  
+      element.on('mousedown', function(event) {
+        var curY = event.pageY;
+        var eleTop = element[0].getBoundingClientRect().top; 
+        if (goodCoordinates(curY, eleTop)) { 
+          event.preventDefault();
+          startX = event.pageX - x;
+          startY = event.pageY - y;
+          $document.on('mousemove', mousemove);
+          $document.on('mouseup', mouseup);
+        }
+      });
+
+      function mousemove(event) {
+        y = event.pageY - startY;
+        x = event.pageX - startX;
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+      }
+    } 
+    return { link : link }; 
 });
